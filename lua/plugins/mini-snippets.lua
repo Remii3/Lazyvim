62a66baccebfd6f4a49d8473a7d045fea4ebe062
Remigiusz Wozniak <remek778790@gmail.com>
2025-08-09 14:30:15 +0200

    Fix snippet scopes

--- lua/plugins/mini-snippets.lua
+++ lua/plugins/mini-snippets.lua
@@ -7,8 +7,6 @@ return {
         snippets = {
           -- Load custom file with global snippets first (adjust for Windows)
           gen_loader.from_file("~/.config/nvim/snippets/global.json"),
-          gen_loader.from_file("~/.config/nvim/snippets/ruby.json"),
-          gen_loader.from_file("~/.config/nvim/snippets/eruby.json"),
 
           gen_loader.from_lang(),
         },
